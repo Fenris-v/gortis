02cc0e50f6ae01bb60ea4f27335b7cdc01ef6127
fenris <Fenris-v>
2021-01-08 16:03:59 +0300

    template for umi. Main and about pages

---
+++ templates/gortis/php/library/gortisPhpExtension.php
@@ -0,0 +1,116 @@
+<?php
+
+class gortisPhpExtension extends ViewPhpExtension
+{
+    /**
+     * Инициализирует общие переменные для шаблонов.
+     *
+     * @param array $variables глобальные переменные запроса
+     * @throws Exception
+     */
+    public function initializeCommonVariables(array $variables)
+    {
+        $templateEngine = $this->getTemplateEngine();
+        $templateEngine->setCommonVar('search-action', $variables['pre-lang'] . '/search/search_do/');
+        $templateEngine->setCommonVar('base', $this->getResourceDirectory());
+        $templateEngine->setCommonVar('pre_lang', $variables['pre-lang']);
+    }
+
+    /**
+     * Возвращает объект настроек сайта
+     *
+     * @return iUmiObject|bool
+     * @throws publicException
+     */
+    public function getSettingsContainer()
+    {
+        static $settingsObj;
+
+        if ($settingsObj) {
+            return $settingsObj;
+        }
+
+        /** @var umiSettings|UmiSettingsMacros $settings */
+        $settings = cmsController::getInstance()->getModule('umiSettings');
+
+        $settingsContainerId = $settings->getIdByCustomId('main');
+
+        return $settingsObj = umiObjectsCollection::getInstance()->getObject($settingsContainerId);
+    }
+
+    /**
+     * Возвращает главное меню
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function getMainMenu(): array
+    {
+        static $menu;
+
+        if ($menu) {
+            return $menu;
+        }
+
+        return $menu = $this->macros('menu', 'draw', ['main']);
+    }
+
+    /**
+     * Сортирует многомерные массивы по значению
+     *
+     * @param array  $array      - массив для сортировки
+     * @param string $keyForSort - ключ массива для сортировки
+     * @param int    $typeSort   - тип сортировки
+     * @return array - отсортированный массив
+     */
+    public function arraySort(array $array, string $keyForSort, int $typeSort = SORT_ASC): array
+    {
+        $value = array_column($array, $keyForSort);
+        array_multisort($value, $typeSort, $array);
+        return $array;
+    }
+
+    /**
+     * Склоняет услово "услуга"
+     *
+     * @param int $count
+     * @return string
+     */
+    public function getDeclination(int $count): string
+    {
+        if (
+            $count % 10 === 0 ||
+            $count % 10 >= 5 && $count % 10 <= 9 ||
+            $count % 100 >= 11 && $count % 100 <= 19
+        ) {
+            return "услуг";
+        }
+
+        if ($count % 10 === 1) {
+            return "услуга";
+        }
+
+        return "услуги";
+    }
+
+    /**
+     * Форматирует дату к виду 01.01.2020
+     *
+     * @param int $time
+     * @return string
+     */
+    public function dateFormatter(int $time): string
+    {
+        return date('d.m.Y', $time);
+    }
+
+    /**
+     * Ссылка на главную
+     *
+     * @return string
+     */
+    public function getHomePageUrl(): string
+    {
+        return $this->getTemplateEngine()->getCommonVar('pre_lang') . '/';
+    }
+}
